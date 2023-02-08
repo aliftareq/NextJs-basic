18758d72d6fd0e0c54f779b0e31385b8b5bf6300
aliftareq <aliftareq@gmail.com>
2023-02-08 09:49:15 +0600

    project changed

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div className='h-screen'>
-            <Image src='/404 page.jpg' width={1440} height={400} />
+            <Image src='/404 page.jpg' className='mx-auto' width={1440} height={400} />
             <Link href='/'><button>Back To Home</button></Link>
         </div>
     );
